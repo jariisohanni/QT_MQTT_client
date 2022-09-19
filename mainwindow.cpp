704183bcbd0ded659305477436c5ff44c86f3a15
jariisohanni <jari.isohanni@gmail.com>
2022-09-19 09:38:45 +0300

    First commit

---
+++ mainwindow.cpp
@@ -0,0 +1,30 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    connect(ui->pushButtonConnect, &QPushButton::released, this, &MainWindow::buttonConnectReleased);
+}
+
+void MainWindow::buttonConnectReleased()
+{
+  //Next we need to connect to the server
+    QMessageBox::information(
+        this,
+        tr("Hi"),
+        tr("Let's connect to server") );
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
