0fdc74035368bff368af92e1215a63697b3bd954
jariisohanni <jari.isohanni@gmail.com>
2022-09-23 09:47:22 +0300

    Version where simple MQTT connection, subscription and posting message is implemented

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,7 +3,7 @@
 #include "mqttmessage.h"
 
 #include <QMessageBox>
-
+#include <QMqttClient>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,34 +11,135 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    //Connecting to user interface elements and their actions
     connect(ui->pushButtonConnect, &QPushButton::released, this, &MainWindow::buttonConnectReleased);
     connect(ui->pushButtonSubscribe, &QPushButton::released, this, &MainWindow::buttonSubsribeToTopic);
+    connect(ui->pushButtonSend, &QPushButton::released, this, &MainWindow::buttonSendToTopic);
 
+    //Clear the messages box
     ui->mqttMessages->clear();
 
-    messagesReceivedMockup();
-
+    //Disable buttons at start
     ui->pushButtonSubscribe->setDisabled(true);
+    ui->pushButtonSend->setDisabled(true);
 
+    m_client = new QMqttClient(this);
 
-    manager = new QNetworkAccessManager(this);
-    connectoToURL("http://www.google.com");
-
+    //Connecting MainWindow functions to MqttClients signal
+    connect(m_client, &QMqttClient::stateChanged, this, &MainWindow::updateStateChange);
+    connect(m_client, &QMqttClient::disconnected, this, &MainWindow::brokerDisconnected);
+    connect(m_client, &QMqttClient::messageReceived, this, &MainWindow::mqttMessageReceived);
 }
 
-
-
-void MainWindow::connectoToURL(QString url) const {
-    auto status = connect(manager, &QNetworkAccessManager::finished,
-                      this, &MainWindow::httpReplyFinished);
-    manager->get(QNetworkRequest(QUrl(url)));
-}
-
-void MainWindow::httpReplyFinished(QNetworkReply *reply)
+MainWindow::~MainWindow()
 {
-    QString answer = reply->readAll();
-     ui->mqttMessages->append(answer);
+    delete m_client;
+    delete ui;
 }
+
+void MainWindow::buttonConnectReleased()
+{
+   ui->pushButtonConnect->setDisabled(true);
+
+    //Next we need to connect to the server
+    m_client->setHostname(ui->lineEditUrl->text());
+    m_client->setPort(1883); //Standard MQTT port
+
+    ui->mqttMessages->append("Connecting to host...");
+
+    //connect(m_client, &QMqttClient::messageReceived, this, &MainWindow::brokerDisconnected);
+
+    m_client->connectToHost();
+
+   /* connect(m_client, &QMqttClient::messageReceived, this, [this](const QByteArray &message, const QMqttTopicName &topic) {
+           const QString content = QDateTime::currentDateTime().toString()
+                       + QLatin1String(" Received Topic: ")
+                       + topic.name()
+                       + QLatin1String(" Message: ")
+                       + message
+                       + QLatin1Char('\n');
+
+           ui->mqttMessages->append(content);
+    });*/
+
+}
+
+void MainWindow::brokerDisconnected()
+{
+    ui->pushButtonSend->setDisabled(true);
+    ui->pushButtonSubscribe->setDisabled(true);
+    ui->pushButtonConnect->setDisabled(false);
+}
+
+//This called everytime MQTT client changes state
+//Possible states are { Disconnected, Connecting, Connected }
+void MainWindow::updateStateChange()
+{
+    const QString content = QDateTime::currentDateTime().toString()
+                    + QLatin1String(": State Change: ")
+                    + QString::number(m_client->state());
+                   // + QLatin1Char('\n');
+
+    ui->mqttMessages->append(content);
+
+    if(m_client->state() == QMqttClient::Connected)
+    {
+
+        //Yes we have connect
+        ui->pushButtonSubscribe->setDisabled(false);
+        ui->pushButtonConnect->setDisabled(true);
+    }
+    else if(m_client->state() == QMqttClient::Disconnected)
+    {
+        brokerDisconnected();
+    }
+}
+
+void MainWindow::buttonSubsribeToTopic()
+{
+
+    auto subscription = m_client->subscribe(ui->lineEditTopic->text());
+    if (!subscription)
+    {
+       QMessageBox::critical(this, QLatin1String("Error"), QLatin1String("Could not subscribe. Is there a valid connection?"));
+    }
+    else
+    {
+        ui->mqttMessages->append("Subscribed to topic");
+        ui->pushButtonSubscribe->setDisabled(true);
+        ui->pushButtonSend->setDisabled(false);
+    }
+}
+
+void MainWindow::buttonSendToTopic()
+{
+    if (m_client->publish(ui->lineEditTopic->text(), ui->lineEditMessage->text().toUtf8()) == -1)
+    {
+       QMessageBox::critical(this, QLatin1String("Error"), QLatin1String("Could not publish message"));
+    }
+    else
+    {
+        ui->mqttMessages->append("Posted message to topic");
+    }
+}
+
+
+void MainWindow::mqttMessageReceived(const QByteArray &message, const QMqttTopicName &topic)
+{
+   const QString content = QDateTime::currentDateTime().toString()
+               + QLatin1String(" Received Topic: ")
+               + topic.name()
+               + QLatin1String(" Message: ")
+               + message
+               + QLatin1Char('\n');
+
+   ui->mqttMessages->append(content);
+}
+
+
+
+
+//THESE HERE ONLY MOCKUPS
 
 void MainWindow::messagesReceivedMockup()
 {
@@ -50,27 +151,11 @@ void MainWindow::messagesReceivedMockup()
     ui->mqttMessages->append(temp);
 }
 
-void MainWindow::buttonConnectReleased()
-{
-   ui->pushButtonConnect->setDisabled(true);
-  //Next we need to connect to the server
-    QMessageBox::information(
-        this,
-        tr("Hi"),
-        tr("Let's connect to server") );
 
-  ui->pushButtonConnect->setDisabled(false);
 
-}
 
-void MainWindow::buttonSubsribeToTopic()
-{
 
-}
 
-MainWindow::~MainWindow()
-{
-    delete manager;
-    delete ui;
-}
+
+
 
--- mainwindow.h
+++ mainwindow.h
@@ -3,10 +3,9 @@
 
 #include <QMainWindow>
 
-#include <QtNetwork/QNetworkAccessManager>
-#include <QtNetwork/QNetworkReply>
-
 class QTextEdit;
+class QMqttClient;
+class QMqttTopicName;
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -19,20 +18,24 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
     void connectoToURL(QString url) const;
-    void httpReplyFinished(QNetworkReply *reply);
-
-    QNetworkAccessManager *manager;
 
 private:
     void messagesReceivedMockup();
 
 private slots:
+      //UI slots
       void buttonConnectReleased();
       void buttonSubsribeToTopic();
+      void buttonSendToTopic();
+
+      //MQTT Related stuff here
+      void updateStateChange();
+      void brokerDisconnected();
+      void mqttMessageReceived(const QByteArray &message, const QMqttTopicName &topic);
 
 private:
     Ui::MainWindow *ui;
+    QMqttClient* m_client;
 };
 #endif // MAINWINDOW_H
